5a2b9817166c2545012f8d2520ec89623a154645
SidathDabare <sidath2007@gmail.com>
2023-02-09 16:59:32 +0100

    fix some css

--- src/pages/Work1.jsx
+++ src/pages/Work1.jsx
@@ -5,16 +5,17 @@ import { Container } from "react-bootstrap"
 import { useSelector } from "react-redux"
 import data from "../data.json"
 import "./Work1.css"
+import OpenInNewIcon from "@mui/icons-material/OpenInNew"
 
 const Work1 = () => {
   const themeColor = useSelector((state) => state.setColor.selectedColor)
   return (
     <div className={themeColor ? "dark" : "light"}>
       <Container className='work1-page-container'>
-        <div className='temp-text'>
+        {/* <div className='temp-text'>
           {" "}
           <h3>Projects will available soon..Thanks</h3>
-        </div>
+        </div> */}
         <div className='work1-page-content'>
           {data.map((item, i) => (
             <div
@@ -24,6 +25,15 @@ const Work1 = () => {
               <div className='work1-page-content-text'>
                 <h2 className='work1-page-content-h2'>{item.title}</h2>
                 <small className='work1-page-content-small'>{item.tec}</small>
+                <small
+                  className='work1-page-content-small-link'
+                  onClick={() => window.open(item.link, "_blank")}>
+                  <span>
+                    {" "}
+                    <OpenInNewIcon />
+                  </span>
+                  <span className='px-2 mt-1'>Go to Website</span>
+                </small>
               </div>
               <div className='work1-page-content-overlay'></div>
             </div>
